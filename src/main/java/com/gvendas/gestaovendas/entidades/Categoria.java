cfc8f9998d1a83a3a0d94140c51242bd8f5c997c
Caio Miguel <caiomiguel78@outlook.com>
2022-04-18 11:02:18 -0300

    commit 1

---
+++ src/main/java/com/gvendas/gestaovendas/entidades/Categoria.java
@@ -0,0 +1,56 @@
+package com.gvendas.gestaovendas.entidades;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "nome")
+    @NotBlank(message = "Nome")
+    @Length(min = 3, max = 50, message = "Nome")
+    private String nome;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, nome);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Categoria)) {
+            return false;
+        }
+        Categoria other = (Categoria) obj;
+        return Objects.equals(codigo, other.codigo) && Objects.equals(nome, other.nome);
+    }
+
+}
---
+++ src/main/java/com/gvendas/gestaovendas/entidades/Produto.java
@@ -0,0 +1,42 @@
+package com.gvendas.gestaovendas.entidades;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
+	private Long codigo;
+	
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "quantidade")
+	private Integer quantidade;
+	
+	@Column(name = "preco_custo")
+	private BigDecimal precoCusto;
+	
+	@Column(name = "preco_venda")
+	private BigDecimal precoVenda;
+	
+	@Column(name = "observacao")
+	private String observacao;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria" , referencedColumnName = "codigo")
+	private Categoria categoria;
+
+}
